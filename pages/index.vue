b818a9b940281b747ea728662ab62649c67755f8
jessesoodesu <js.rattlesnake@gmail.com>
2023-03-01 15:22:04 +0800

    Show API content

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <!-- <div>{{ response.details.ext_2 }}</div> -->
+    <div>{{ response.details.ext_2 }}</div>
     <div>Index</div>
     <div>'{{ url }}'</div>
   </div>
@@ -10,15 +10,14 @@
 export default {
   name: "IndexPage",
   async asyncData({ $axios, app }) {
-    // try {
-    //   const response = await $axios.$get(
-    //     process.env.BASE_URL + "/rcms-api/4/mission-statement/4"
-    //   );
-    //   return { response };
-    // } catch (e) {
-    //   console.log(e.message);
-    // }
-    return { url: process.env.BASE_URL + "/rcms-api/4/mission-statement/4" };
+    try {
+      const response = await $axios.$get(
+        process.env.BASE_URL + "/rcms-api/4/mission-statement/4"
+      );
+      return { response };
+    } catch (e) {
+      console.log(e.message);
+    }
   },
 };
 </script>
