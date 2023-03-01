b557f94b479d514727480b4e6e708671b1eb30dd
jessesoodesu <js.rattlesnake@gmail.com>
2023-03-01 11:36:16 +0800

    Removes API call to make sure the site loads

--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,22 @@
 <template>
   <div>
-    <div>{{ response.details.ext_2 }}</div>
+    <!-- <div>{{ response.details.ext_2 }}</div> -->
+    <div>Index</div>
   </div>
 </template>
 
 <script>
 export default {
   name: "IndexPage",
-  async asyncData({ $axios, app }) {
-    try {
-      const response = await $axios.$get(
-        process.env.BASE_URL + "/rcms-api/4/mission-statement/4"
-      );
-      return { response };
-    } catch (e) {
-      console.log(e.message);
-    }
-  },
+  // async asyncData({ $axios, app }) {
+  //   try {
+  //     const response = await $axios.$get(
+  //       process.env.BASE_URL + "/rcms-api/4/mission-statement/4"
+  //     );
+  //     return { response };
+  //   } catch (e) {
+  //     console.log(e.message);
+  //   }
+  // },
 };
 </script>
